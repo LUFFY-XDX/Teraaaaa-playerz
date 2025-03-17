bbc41cd0c343a74f17864f5697c582360f3d1ddb
LUFFY-XDX <emotesmlbb@gmail.com>
2025-03-17 09:56:31 +0545

    Update settings.py

--- video_link_project/settings.py
+++ video_link_project/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATICFILES_DIRS = [
-    BASE_DIR / 'video_app' / 'static',
+    os.path.join(BASE_DIR, 'video_app', 'static'),
 ]
-
 STATIC_URL = '/static/'
 
 # Default primary key field type
