368bd52bebf159a0c0e1f65284da88981cfd9310
LUFFY-XDX <emotesmlbb@gmail.com>
2025-03-17 10:13:34 +0545

    Update settings.py

--- video_link_project/settings.py
+++ video_link_project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-i9jb2a6let1^mc2#5v=n(9_(yc944a_%1vkoc171)c*)5wv_-)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["tera-vision.onrender.com"]
+ALLOWED_HOSTS = ["cyberflix.onrender.com"]
 
 
 # Application definition
