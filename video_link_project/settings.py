7466e0d993d002b0b46307c358e372fdaa7257e1
LUFFY-XDX <emotesmlbb@gmail.com>
2025-03-17 09:31:08 +0545

    Update settings.py

--- video_link_project/settings.py
+++ video_link_project/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'video_app' / 'static',
 ]
 
-STATIC_URL = 'video_app/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
